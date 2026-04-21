88516c7026233ed269613cd965929f04c5701953
Tyler Bertrand <tylerbertrand44@gmail.com>
2026-04-20 22:36:56 -0400

    fixed error for login page

--- client/src/LoginPage.jsx
+++ client/src/LoginPage.jsx
@@ -1,10 +1,12 @@
+import musicImg from './assets/music-recommender-png.png'
+
 const LoginPage = () => {
   return (
     <div>
       <h1>
         LOGIN PAGE
       </h1>
-      <img src='./assets/music-recommender-png.png'></img>
+      <img src={musicImg} style={{ width: '300px' }}></img>
       <button onClick={() => window.location.href = '/login'}>
         Login with Spotify
       </button>
